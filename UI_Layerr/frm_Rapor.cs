e1163b3fefcc104598fd84ccd4aa73234fc261dc
Onur Köse <103755656+Onur-Kose@users.noreply.github.com>
2023-02-06 19:30:17 +0300

    hata düzeltmeleri

--- UI_Layerr/frm_Rapor.cs
+++ UI_Layerr/frm_Rapor.cs
@@ -33,17 +33,45 @@ namespace UI_Layerr
             rjComboBox1.Items.Add("Öğle yemeğinde en çok yediklerim");
             rjComboBox1.Items.Add("Akşam yemeğinde en çok yediklerim");
             rjComboBox1.Items.Add("Ara Öğünlerde en çok yediklerim");
+            rjComboBox1.Items.Add("tüm zamanlarda en çok yediklerim");
+
+            float Toplam = 0;
+            var que = db.Meals.Where(x => x.UserDetailID == GelenID && x.TüketimTarihi.Year == rjDatePicker1.Value.Year && x.TüketimTarihi.Month == rjDatePicker1.Value.Month && x.TüketimTarihi.Day == rjDatePicker1.Value.Day).ToList();
+            foreach (var item in que)
+            {
+                Toplam += item.TopKcall;
+            }
+            label1.Text = Toplam.ToString();
         }
         DateTime Bugün = DateTime.Now;
         private void rjButton1_Click(object sender, EventArgs e)
         {
             try
             {
-                var query = db.Meals.Where(x => x.UserDetailID == GelenID && x.TüketimTarihi.Date == rjDatePicker1.Value.Date).ToList();
-                dataGridView1.DataSource = query;
+                if (rjComboBox1.SelectedIndex == 0)
+                {
+ 
+                }
+                else if (rjComboBox1.SelectedIndex == 1)
+                {
 
-                var que = db.Meals.Where(x => x.UserDetailID == GelenID && x.TüketimTarihi.Date == rjDatePicker1.Value.Date).Sum(x => x.TopKcall);
-                label1.Text = que.ToString();
+                }
+                else if (rjComboBox1.SelectedIndex == 2)
+                {
+
+                }
+                else if (rjComboBox1.SelectedIndex == 3)
+                {
+
+                }
+                else if (rjComboBox1.SelectedIndex == 4)
+                {
+                    var query = db.Meals.Where(x => x.UserDetailID == GelenID && x.TüketimTarihi.Year == rjDatePicker1.Value.Year && x.TüketimTarihi.Month == rjDatePicker1.Value.Month && x.TüketimTarihi.Day == rjDatePicker1.Value.Day).GroupBy(x => x.FoodInfoID).FirstOrDefault().Count();
+                    
+                    dataGridView1.DataSource = query;
+                }
+ 
+
             }
             catch (Exception ex)
             {
